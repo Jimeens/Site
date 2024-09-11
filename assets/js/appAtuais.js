caf4d0b0ddfac7a63e7e724934ff266c6c93336f
Jimeens <lcsximenes@usp.br>
2024-09-11 10:01:43 -0300

    main

--- assets/js/appAtuais.js
+++ assets/js/appAtuais.js
@@ -1,20 +1,4 @@
-const projetosAtuais = [
-    {
-        titulo: "Livro",
-        desc: `Em adição à umas das minhas iniciações científicas, foi colocada em prática o processo de escrever um livro/apostila sobre mecânica quântica.`,
-        link: "#"
-    },
-    {
-        titulo: "Site (One Piece)",
-        desc: `Afim de aprender HTML5, CSS e Javascript, comecei a produzir um simples site de One Piece com estas tecnologias envolvidas para estudar.`,
-        link: "#"
-    },
-    {
-        titulo: "Site (Pessoal)",
-        desc: `Tanto para armazenar informações profissionais quanto estudar um pouco de front-end, decidi produzir meu próprio site (este mesmo).`,
-        link: "#"
-    }
-]
+import {projetosAtuais} from "./DataAtuais.js";
 
 const cardAtual = document.querySelector(".container-environment");
 const searchAtual = document.querySelector("#searchAtual");
